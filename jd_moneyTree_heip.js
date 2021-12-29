ce2aaec9e52e39400d42363d65f2010499e2d1ed
leastfrog <41647217+leastfrog@users.noreply.github.com>
2021-12-29 15:33:45 +0800

    Update jd_moneyTree_heip.js

--- jd_moneyTree_heip.js
+++ jd_moneyTree_heip.js
@@ -216,10 +216,15 @@ function shareCodesFormat() {
   return new Promise(async resolve => {
     $.newShareCodes = [];
     let inviteCodes = [
-    	''
+      'RIxPIOnSKiDhVcdgX7LvPg@iGBnlkHmUZMEBGXASi_VjMAdoUJQ3Dik@4JRERA4EOLkSEMqTeNdtGw@RxnoGLOsKxpRFWYJEqQSAA',
+      'RIxPIOnSKiDhVcdgX7LvPg@iGBnlkHmUZMEBGXASi_VjMAdoUJQ3Dik@4JRERA4EOLkSEMqTeNdtGw@RxnoGLOsKxpRFWYJEqQSAA'
     ];
     if ($.shareCodesArr[$.index - 1]) {
       $.newShareCodes = $.shareCodesArr[$.index - 1].split('@');
+    } else {
+      console.log(`由于您第${$.index}个京东账号未提供shareCode,将为本脚本作者【zero205】助力\n`)
+      const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
+      $.newShareCodes = inviteCodes[tempIndex].split('@');
     }
     console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify($.newShareCodes)}`)
     resolve();
